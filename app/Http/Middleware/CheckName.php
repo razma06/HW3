b5fc6911a02b948c506f93eb1d59bfe38ed0dccd
razma06 <giorgi.razmadze.1@gmail.com>
2021-12-25 20:52:31 +0400

    first commit

---
+++ app/Http/Middleware/CheckName.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckName
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if($request->input('name') == 'my-super-middleware'){
+            return $next($request);
+        }else{
+            return redirect('/error');
+        }
+    }
+}
